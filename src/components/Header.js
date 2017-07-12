c63d16dd0ae04b4149515e9f97b48818927a110e
Karim Cheurfi <karim.cheurfi@gmail.com>
2017-07-12 14:41:27 +0000

    initial commit

---
+++ src/components/Header.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import './Header.css'
+import Background from './images/gist.gif'
+
+let headerStyles = {
+    backgroundImage: `url(${Background})`
+}
+
+class Header extends Component {
+
+  render() {
+
+    return (
+
+      <div className="header">
+
+        <header style={headerStyles} className="hero">
+        </header>
+
+      </div>
+
+    );
+  }
+}
+
+export default Header;
+
