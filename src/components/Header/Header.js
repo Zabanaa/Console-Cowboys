1d54077bdbe467e988b4fd8ed444b33edffd4f31
Karim Cheurfi <karim.cheurfi@gmail.com>
2017-07-12 14:45:44 +0000

    rearrange component files into their own folder

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './Header.css'
-import Background from './images/gist.gif'
+import Background from '../images/gist.gif'
 
 let headerStyles = {
     backgroundImage: `url(${Background})`
