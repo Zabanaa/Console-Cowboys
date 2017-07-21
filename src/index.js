9fce45010c6f223e51996e04740b4ecd6a246104
Karim Cheurfi <karim.cheurfi@gmail.com>
2017-07-21 15:45:48 +0000

    add /publish /preview routes

--- src/index.js
+++ src/index.js
@@ -1,18 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Route, BrowserRouter } from 'react-router-dom'
-import Main from './App';
+import App from './App';
 import Terms from './pages/Terms';
 import Privacy from './pages/Privacy';
+import Publish from './pages/Publish';
+import Preview from './pages/Preview';
 import './index.css';
 
 ReactDOM.render(
 
     <BrowserRouter>
         <div>
-            <Route exact path="/" component={Main} />
+            <Route exact path="/" component={App} />
             <Route exact path="/terms" component={Terms} />
             <Route exact path="/privacy" component={Privacy} />
+            <Route exact path="/publish" component={Publish} />
+            <Route exact path="/preview" component={Preview} />
         </div>
     </BrowserRouter>,
 
