bb089237d327eb3069250b7f122da474f85fd22b
Karim Cheurfi <karim.cheurfi@gmail.com>
2017-07-21 09:29:33 +0000

    create Logo component

---
+++ src/components/Logo.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react'
+
+class Logo extends Component {
+
+    render() {
+
+        <header className="post-job__header">
+
+            <a href="/" className="logo post-job__logo">
+
+                <p>
+                    <span>Console</span>
+                    <span>Cowboys</span>
+                </p>
+
+            </a>
+
+        </header>
+
+    }
+}
+
+export default Logo
