5b93b9055531896737957b3f19472afad107ccab
Karim Cheurfi <karim.cheurfi@gmail.com>
2017-07-21 10:17:54 +0000

    add router logic

--- src/index.js
+++ src/index.js
@@ -1,6 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import { Route, BrowserRouter } from 'react-router-dom'
+import Main from './App';
+import Terms from './pages/Terms';
+import Privacy from './pages/Privacy';
 import './index.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+
+    <BrowserRouter>
+        <div>
+            <Route exact path="/" component={Main} />
+            <Route exact path="/terms" component={Terms} />
+            <Route exact path="/privacy" component={Privacy} />
+        </div>
+    </BrowserRouter>,
+
+    document.getElementById('root')
+
+);
