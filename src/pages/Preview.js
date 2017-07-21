e301a810e170ca98426ad6ee0011605852dc3408
Karim Cheurfi <karim.cheurfi@gmail.com>
2017-07-21 10:35:00 +0000

    create blank Preview and Publish pages

---
+++ src/pages/Preview.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+class Preview extends Component {
+
+    render() {
+
+        return (
+
+
+        )
+    }
+}
+
+export default Preview
---
+++ src/pages/Publish.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+
+class Publish extends Component {
+
+    render() {
+
+        return (
+
+
+        )
+    }
+
+}
+
+export default Publish
+
