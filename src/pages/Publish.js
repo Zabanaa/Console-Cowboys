a02e80d9d5da549d311a084870763ae47031aba8
Karim Cheurfi <karim.cheurfi@gmail.com>
2017-07-21 15:45:31 +0000

    create publish view

--- src/pages/Publish.js
+++ src/pages/Publish.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react'
+import Logo from '../components/Logo'
+import PublishForm from '../components/PublishForm'
 
 class Publish extends Component {
 
@@ -6,10 +8,24 @@ class Publish extends Component {
 
         return (
 
+            <main className="post-job">
 
+                <div className="post-job__container">
+
+                    <Logo />
+
+                    <div className="post-job__main-heading">
+                        <h1>Post your Job</h1>
+                        <p>Ads are just $79 and run for 30 Days.</p>
+                    </div>
+
+                    <PublishForm />
+
+                </div>
+
+            </main >
         )
     }
-
 }
 
 export default Publish
