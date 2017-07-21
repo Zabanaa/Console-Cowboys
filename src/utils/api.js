f6fb2352b3a57112a9cdd78999441cc29c2257fe
Karim Cheurfi <karim.cheurfi@gmail.com>
2017-07-21 09:30:03 +0000

    create getJobByContractType and getRemoteJobs helpers

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,13 +1,28 @@
 import axios from 'axios'
 
-const getAllCharacters = () => {
+let apiUri  = "https://consolecowboys.club/jobs"
 
-    let apiUri  = "https://consolecowboys.club/jobs"
+const getAllJobs = () => {
+
 
     return axios.get(apiUri)
         .then( response => response )
         .catch(err => console.error(err))
 }
 
-export default { getAllCharacters }
+const getJobByContractType = contractType => {
+
+    return axios.get(`${apiUri}/${contractType}`)
+        .then(response => response)
+        .catch(err => console.error(err))
+}
+
+const getRemoteJobs = () => {
+
+    return axios.get(`${apiUri}?remote=true`)
+        .then(response => response)
+        .catch(err => console.error(err))
+}
+
+export default { getAllJobs, getJobByContractType, getRemoteJobs }
 
